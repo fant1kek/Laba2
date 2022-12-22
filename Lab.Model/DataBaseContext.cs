778945124ddea74466cf08f4281e3f979604c978
fant1kek <82901435+fant1kek@users.noreply.github.com>
2022-12-23 00:46:20 +0300

    public

---
+++ Lab.Model/DataBaseContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore.SqlServer;
+using Microsoft.EntityFrameworkCore;
+
+namespace Lab.Model
+{
+    public class DataBaseContext : DbContext
+    {
+        //public DataBaseContext()
+        //{
+        //    Database.EnsureCreated();
+        //}
+        protected override void OnConfiguring(DbContextOptionsBuilder _context)
+        {
+            _context.UseSqlServer("Server=ASUS;DataBase=laba2;Trusted_Connection=True;TrustServerCertificate=True;");
+        }
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
---
+++ WPFWindow/MainWindow.xaml.cs
@@ -0,0 +1,65 @@
+﻿using Lab.Model;
+using Laba.BusinessLogic.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPFWindow
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        EmployeeService _employeeService;
+        public MainWindow()
+        {
+            InitializeComponent();
+            _employeeService = new EmployeeService();
+            laba2.ItemsSource = _employeeService.Get();
+        }
+        private void AddNewEmployee_Clicked(object sender, RoutedEventArgs e)
+        {
+            AddEmployee win = new AddEmployee();
+            win.Show();
+        }
+        private void UpdateGrid()
+        {
+            laba2.ItemsSource = null;
+            laba2.ItemsSource = _employeeService.Get();
+        }
+        private void ButtonDelete_Click(object sender, RoutedEventArgs e)
+        {
+
+            if (laba2.SelectedItems.Count > 0 && MessageBox.Show("Подтвердить удаление", "Удаление", MessageBoxButton.YesNo) == MessageBoxResult.Yes)
+            {
+                for (int i = 0; i < laba2.SelectedItems.Count; i++)
+                {
+                    Employee employee = laba2.SelectedItems[i] as Employee;
+                    if (employee != null)
+                    {
+                        _employeeService.Deleted(employee);
+                    }
+                }
+            }
+            UpdateGrid();
+
+        }
+        private void DataGrid_CellEditEnding(object sender, DataGridCellEditEndingEventArgs e)
+        {
+            _employeeService.Update(e.Row.Item as Employee);
+            UpdateGrid();
+        }
+    }
+}
